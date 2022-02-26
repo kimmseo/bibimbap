56e6ac8f852d74aa86465e5071bddf424b0d49fc
kimmseo <kimms010625@gmail.com>
2022-02-26 16:38:35 +0800

    added PinCodeTextField page

--- admin/lib/main.dart
+++ admin/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -77,10 +60,71 @@ class _MyHomePageState extends State<MyHomePage> {
             Map<Permission, PermissionStatus> statuses =
                 await [Permission.locationWhenInUse].request();
             print(statuses[Permission.locationWhenInUse]);
+
+            if (await Permission.locationWhenInUse.request().isGranted) {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: ((context) => (PinCodeVerificationScreen()))));
+            }
+            ;
           },
           label: const Text('Click here to grant Location permissions'),
         ),
       ),
     );
+  }
+}
+
+class PinCodeVerificationScreen extends StatefulWidget {
+  @override
+  _PinCodeVerificationScreenState createState() =>
+      _PinCodeVerificationScreenState();
+}
+
+class _PinCodeVerificationScreenState extends State<PinCodeVerificationScreen> {
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Material App',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Material App Bar'),
+            ),
+            body: Center(
+                child: PinCodeTextField(
+              appContext: context,
+              length: 6,
+              obscureText: false,
+              animationType: AnimationType.fade,
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(5),
+                fieldHeight: 50,
+                fieldWidth: 40,
+                activeFillColor: Colors.white,
+              ),
+              animationDuration: Duration(milliseconds: 300),
+              backgroundColor: Colors.blue.shade50,
+              enableActiveFill: true,
+              controller: textEditingController,
+              onCompleted: (v) {
+                print("Completed");
+              },
+              onChanged: (value) {
+                print(value);
+                //setState(() {
+                //currentText = value;
+                //});
+              },
+              beforeTextPaste: (text) {
+                print("Allowing to paste $text");
+                //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                return true;
+              },
+            ))));
   }
 }
